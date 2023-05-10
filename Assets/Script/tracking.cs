058f4b79a3f629d31ce5d69711000f808ddaf952
Yuxin Wen <ywen@umd.edu>
2023-05-09 22:56:32 -0400

    navie raction time

--- Assets/Script/tracking.cs
+++ Assets/Script/tracking.cs
@@ -12,9 +12,12 @@ public class tracking : MonoBehaviour
     
     public GameObject camera;
     public GameObject lookingAtDebug;
+    public GameObject cat;
     
     public Vector3 localHit = Vector3.zero;
-    
+
+    public float reactionTime = -1;
+    public float reactionTimeCounter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,8 +39,10 @@ public class tracking : MonoBehaviour
                 string json = JsonUtility.ToJson(trackingData);
 
                 StreamWriter writer = new StreamWriter(Application.persistentDataPath + "/tracking_data.txt", true);
+                // StreamWriter writer = new StreamWriter("Assets/tracking_data.txt", true);
 
                 writer.WriteLine("Total time: " + totalTime);
+                writer.WriteLine("Reaction time: " + reactionTime);
 
                 foreach (var currObject in allObjects)
                 {
@@ -74,5 +79,30 @@ public class tracking : MonoBehaviour
             }
             totalTime += 1;
         }
+
+        // check if seen the cat
+        if (reactionTime == -1)
+        {
+            RaycastHit hit;
+            Vector3 pos = camera.transform.position;
+            Vector3 forward = camera.transform.forward;
+
+            if (Physics.Raycast(pos, forward, out hit, Mathf.Infinity))
+            {
+                localHit = hit.point;
+                //Debug.DrawRay(pos, forward * 100, Color.green);
+                lookingAtDebug = hit.collider.gameObject;
+                string hitObjectName = lookingAtDebug.name;
+                if (Vector3.Distance(cat.transform.position, localHit) <= 1.2)
+                {
+                    reactionTime = reactionTimeCounter;
+                    Debug.Log("Seen!");
+                }
+            }
+
+            reactionTimeCounter += 1;
+        }
+
+        totalTime += 1;
     }
 }
